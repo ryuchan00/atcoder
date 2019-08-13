0f99bdcd07af982759feda1cac0cce539c146c00
ryuchan00 <leokun0210@gmail.com>
2019-08-13 19:14:49 +0900

    update

--- welcom_to_atcoder.rb
+++ welcom_to_atcoder.rb
@@ -1,8 +1,8 @@
 # 整数の入力
 a = gets.to_i
-p a
 # スペース区切りの整数の入力
-#b,c=gets.chomp.split(" ").map(&:to_i);
+b, c = gets.chomp.split(" ").map(&:to_i)
 # 文字列の入力
-#s = gets.chomp
+s = gets.chomp
 # 出力
+puts "#{a + b + c} #{s}"
