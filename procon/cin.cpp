9b96a7e7cacbee0f081a83dae844f91b142e2b35
ryuchun00 <leokun0210@gmail.com>
2022-07-20 01:33:49 +0900

    update

--- procon/cin.cpp
+++ procon/cin.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main() {
   // char a, b;
-  int a, b;
-  cin >> a >> b;
+  // int a, b;
+  // cin >> a >> b;
+  char a;
+  cin >> a;
+  cin >> a;
 
   cout << a << endl;
-  cout << b << endl;
+  // cout << b << endl;
   return 0;
 }
