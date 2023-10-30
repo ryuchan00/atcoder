25868c4d64cdb8e0b638c8319ae880f08004013b
ryuchun00 <leokun0210@gmail.com>
2023-10-30 19:21:37 +0900

    update

---
+++ past/past201912_i.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <cassert>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+#define rep(i, n) for (int i = 0; (i) < (n); ++(i))
+
+bool has_bit(int n, int i) {
+  return (n & (1 << i)) > 0;
+}
+
+// https://atcoder.jp/contests/past201912-open/tasks/past201912_g
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  // 1始まりにするためにダミーを入れる
+  vector<ll> S(1, 0);
+  vector<ll> C(1, 0);
+  rep(i, M) {
+    string s;
+    ll c;
+    cin >> s >> c;
+    ll s_val = 0;
+    rep(j, N) {
+      if (s[j] == 'Y') {
+        s_val = (s_val | 1 << j);
+      }
+    }
+    S.push_back(s_val);
+    C.push_back(c);
+  }
+
+  // rep(i, S.size()) {
+  //   /*
+  //   サンプルデータ1の場合
+  //   0000 0
+  //   0111 100
+  //   0011 20
+  //   0101 10
+  //   0110 25
+  //   */
+  //   bitset<4> b1(S[i]);
+  //   cout << b1 << " ";
+  //   cout << C[i] << endl;
+  // }
+
+  // 集合としてあり得るものの個数．2**Nでも同じ
+  int ALL = 1 << N;
+
+  // cost[i][j]:セットiまで見て揃った部品の集合がnであるときのコスト最小値
+  vector<vector<ll>> cost(M + 1, vector<ll>(ALL, 1e18));
+
+  // 初期条件
+  cost[0][0] = 0;
+
+  // iが小さいところから順に計算
+  for (ll i = 1; i < M + 1; i++) {
+    for (ll n = 0; n < ALL; n++) {
+      // セットiを買わない
+      cost[i][n] = min(cost[i][n], cost[i - 1][n]);
+      // セットiを買う
+      cost[i][(n | S[i])] = min(cost[i][(n | S[i])], cost[i - 1][n] + C[i]);
+    }
+  }
+
+  // rep(i, cost.size()) {
+  //   rep(j, cost[i].size()) {
+  //     cout << cost[i][j] << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // 答えは部品が全部そろっている状態の最小コスト
+  // ただし1e18(=初期値より変化なし)
+  ll ans = cost[M][ALL - 1];
+  if (ans == 1e18) {
+    ans = -1;
+  }
+
+  cout << ans << endl;
+}
